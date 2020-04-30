c4c3b9d68a30c57c1b75b300b252bdce86bd9c47
ZtereoHYPE <lorenzodegrandis@icloud.com>
2020-04-30 15:30:53 +0200

    changes and reorganization of javascript

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-var contactButton = document.getElementById('contactButton'); //creates contact button variable
-var homeButton = document.getElementById('homeButton'); //creates home button variable
-var homeCounter = 0;  //creates a counter
-var introButton = document.getElementById('introButton'); //creates intro button variable
-var myEquipmentButton = document.getElementById('myEquipmentButton'); //creates equipment button variable
-var myWorkButton = document.getElementById('myWorkButton'); //creates work variable
-var easterButton = document.getElementById('easterButton'); // creates easter egg button variable
-var mybutton = document.getElementById("backToTopButton");  // creates my button variable
-var checkbox = document.getElementById("checkbox"); // creates checkbox variable
-var header = document.getElementById("header"); // creates header variable
-var myWorkDiv = document.getElementById("myWorkDiv")
-var myEquipmentDiv = document.getElementById("myEquipmentDiv")
-introButton.addEventListener('click', () => { //scrolls the screen on click
+//declare variables
+var homeCounter = 0;
+var easterButton = document.getElementById('easterButton');
+var backToTopButton = document.getElementById("backToTopButton");
+
+//make buttons scroll to div + easter egg
+document.getElementById('introButton').addEventListener('click', () => { 
   document.getElementById('introDiv').scrollIntoView();
 });
-myWorkButton.addEventListener('click', () => {  //scrolls the screen on click
-  document.getElementById('myWorkDiv').scrollIntoView();
+document.getElementById('myWorkButton').addEventListener('click', () => {  
+document.getElementById('myWorkDiv').scrollIntoView();
 });
-myEquipmentButton.addEventListener('click', () => { //scrolls the screen on clickv
+document.getElementById('myEquipmentButton').addEventListener('click', () => { 
   document.getElementById('myEquipmentDiv').scrollIntoView();
 });
-contactButton.addEventListener('click', () => { //scrolls the screen on click
+document.getElementById('contactButton').addEventListener('click', () => { 
   document.getElementById('contactDiv').scrollIntoView();  
 });
-easterButton.addEventListener('click', () => {  //takes to never gonna gibe you up video on click
+easterButton.addEventListener('click', () => {
   window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 });
-homeButton.addEventListener('click', () => {  //count how many times you click home and makes the easter egg appear after 10 clicks
+
+//count n° times home is clicked for easter egg
+var homeButton = document.getElementById('homeButton').addEventListener('click', () => {
   homeCounter += 1;
   if (homeCounter > 10) {
     easterButton.style.display = "inline-block";
@@ -33,17 +29,21 @@ homeButton.addEventListener('click', () => {  //count how many times you click h
       easterButton.style.display = "none";
     }   
 }); 
-window.onscroll = function() {scrollFunction()};  //makses the back to top button appear when you scroll more than 200 pixels
+
+//makes back to top button appear on scroll
+window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "block";
+    document.getElementById("backToTopButton").style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    document.getElementById("backToTopButton").style.display = "none";
   }
 }
-function topFunction() {  //creates the scroll back to top function
-  document.body.scrollTop = 0;  // For Safari
-  document.documentElement.scrollTop = 0; // For all other browsers i guess
+
+//declares back to top function
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 if(window.innerHeight > window.innerWidth){
   alert("This page is best viewed in landscape mode and on tablets/desktops.");
